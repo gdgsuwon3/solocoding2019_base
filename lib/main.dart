d5a37035bdf643ade9fa7619d6c3e6b8ee8a0e0f
ny0011 <ny0011@github.com>
2019-04-09 18:30:38 +0900

    execute HELLO WORLD

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 // This widget is the root of your application.
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => new MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // set material design app
-    return MaterialApp(
-      title: 'solocoding2019', // application name
+    return new MaterialApp(
+      title: 'welcome to Flutter', // application name
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
+      home: new Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Demo'), // app bar title
+          title: const Text('Flutter Demo'), // app bar title
         ),
-        body: Center(
-          child: Text('Hello, world'), // center text
+        body: const Center(
+          child: const Text('Hello, world'), // center text
         ),
       ),
     );
